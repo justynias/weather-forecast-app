58926436c9845725f6e68a99198c659989ac0326
justynias <justynakozik1@gmail.com>
2019-03-11 15:30:09 +0100

    connected to api with json deserialization

--- Weather/app/src/main/java/com/just/weatherforecast/ui/MainActivity.kt
+++ Weather/app/src/main/java/com/just/weatherforecast/ui/MainActivity.kt
@@ -1,14 +1,22 @@
-package com.just.weatherforecast
+package com.just.weatherforecast.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import com.just.weatherforecast.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+         // only for testing
+//        val apiService = WeatherApiService()
+//        GlobalScope.launch(Dispatchers.Main) {
+//            val currentWeatherResponse = apiService.getCurrentWeather("London").await()
+//            Log.d("API_EXAMPLE", currentWeatherResponse.mainWeatherParameters.toString())
+//        }
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.options_menu, menu)
